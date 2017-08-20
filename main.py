a0b24015c55f62b9b04760f070c2f23011967d84
marnold <michael.arnold@adtran.com>
2017-08-19 19:59:11 -0500

    minor changes

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 from dataset.dataset import MnistDataset
 import math
 import pandas as pd
+import tarfile
+import gzip
+import os.path
 
 n_hidden_1 = 800 # 1st layer number of features
 n_hidden_2 = 200 # 2nd layer number of features
@@ -15,32 +18,25 @@ Y_ = tf.placeholder(tf.float32, [None, n_classes])
 lr = tf.placeholder(tf.float32)
 pkeep = tf.placeholder(tf.float32)
 
-#layer = tf.layers.dense(X, 255, activation=tf.nn.relu)
-#layer = tf.nn.dropout(layer, pkeep)
-
 weights = {
     'w1': tf.Variable(tf.random_normal([n_input, n_hidden_1]), name='W1'),
-    'w2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]), name='W2'),
-    'w3': tf.Variable(tf.random_normal([n_hidden_2, n_classes]), name='W3'),
-    'w4': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]), name='W4')
+#    'w2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]), name='W2'),
+    'w3': tf.Variable(tf.random_normal([n_hidden_1, n_classes]), name='W3'),
+#    'w4': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]), name='W4')
 }
 biases = {
     'b1': tf.Variable(tf.random_normal([n_hidden_1]), name='b1'),
-    'b2': tf.Variable(tf.random_normal([n_hidden_2]), name='b2'),
+#    'b2': tf.Variable(tf.random_normal([n_hidden_2]), name='b2'),
     'b3': tf.Variable(tf.random_normal([n_classes]), name='b3'),
-    'b4': tf.Variable(tf.random_normal([n_hidden_2]), name='b4')
+#    'b4': tf.Variable(tf.random_normal([n_hidden_2]), name='b4')
 }
 
 
+#Single hidden layer
 layer_1 = tf.add(tf.matmul(X, weights['w1']), biases['b1'])
 layer_1 = tf.nn.relu(layer_1)
 
-layer_2 = tf.add(tf.matmul(layer_1, weights['w2']), biases['b2'])
-layer_2 = tf.nn.relu(layer_2)
-
-#layer_3 = tf.add(tf.matmul(layer_2, weights['w4']), biases['b4'])
-#layer_3 = tf.nn.relu(layer_3)
-layer_2 = tf.nn.dropout(layer_2, pkeep)
+layer_2 = tf.nn.dropout(layer_1, pkeep)
 Ylogits = tf.add(tf.matmul(layer_2, weights['w3']), biases['b3'])
 Y = tf.nn.softmax(Ylogits)
 
@@ -57,6 +53,21 @@ init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
+if( not os.path.isfile('test/test.csv')):
+    with gzip.open('test/test.csv.gz', 'rb') as f:
+        file_content = f.read()
+
+    with open('test/test.csv', 'wb') as f:
+        f.write(file_content)
+
+if(not os.path.isfile('test/train.csv')):
+    with gzip.open('test/train.csv.gz', 'rb') as f:
+        file_content = f.read()
+
+    with open('test/train.csv', 'wb') as f:
+        f.write(file_content)
+
+
 data = MnistDataset(test_filename = 'test/test.csv', train_filename='test/train.csv')
 for i in range(1,5000):
     # data.train.next_batch    max_learning_rate = 0.003
@@ -70,8 +81,6 @@ for i in range(1,5000):
 
     #batch_Y = batch_Y.reshape((100,batch_Y.shape[1]))
     a, c = sess.run([accuracy, cross_entropy], {X: batch_X, Y_: batch_Y, pkeep: 1.0})
-    if a > 0.999:
-        break
     print(str(data.train.epoch) + ": training accuracy:" + str(a) + " training loss: " + str(c) )
     sess.run(train_step, {X: batch_X, Y_: batch_Y, lr: learning_rate, pkeep: 0.75})
 
