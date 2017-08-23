aa55f56d5e375e19895c65cff83297b809182e1a
mike <mike@debian>
2017-08-23 15:37:58 -0500

    ...

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,6 +1,34 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+from scipy import ndimage
+from scipy.ndimage.interpolation import map_coordinates
+from scipy.ndimage.filters import gaussian_filter
+import numpy
+import matplotlib.pyplot as plt
+
+def elastic_transform(image, alpha=34, sigma=8, random_state=None):
+    """Elastic deformation of images as described in [Simard2003]_.
+ 
+ 
+    .. [Simard2003] Simard, Steinkraus and Platt, "Best Practices for
+       Convolutional Neural Networks applied to Visual Document Analysis", in
+       Proc. of the International Conference on Document Analysis and
+       Recognition, 2003.
+    """
+    if random_state is None:
+        random_state = numpy.random.RandomState(None)
+ 
+    shape = image.shape
+    dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
+    dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
+ 
+    x, y = numpy.meshgrid(numpy.arange(shape[0]), numpy.arange(shape[1]))
+    indices = numpy.reshape(y+dy, (-1, 1)), numpy.reshape(x+dx, (-1, 1))
+ 
+    new_image = map_coordinates(image, indices, order=1).reshape(shape)
+    return new_image
+
 class BatchSizeOutOfBoundsException(Exception):
     def __init__(self, *dErrorArguments):
         Exception.__init__(self, "Batch Size was larger than dataset size {}"
@@ -30,11 +58,46 @@ def dataset_from_csv(filename, train = False):
 
 
 class Dataset(object):
-    def __init__(self, inputs, labels):
+    def __init__(self, inputs, labels, expand=False, random = False):
         self.__inputs = np.array(inputs)
         self.__labels = np.array(labels)
         self.__index_in_epoch = 0
         self.__epochs_completed = 0
+
+        expanded_examples = []
+        expanded_labels = []
+        tot = len(self.__inputs)
+        i = 0
+        if(expand):
+            for x,y in zip(self.__inputs, self.__labels):
+                print i / float(tot)
+                i = i + 1
+                bg_value = np.median(x)
+                shape = x.shape
+                image = x.reshape(28,28)
+                for _ in range(8):
+                    angle = np.random.randint(-10,10,1)
+                    new_image = ndimage.rotate(image, angle, reshape=False, cval=bg_value)
+                    shift = np.random.randint(-2,2,2)
+                    new_image_ = ndimage.shift(new_image, shift, cval=bg_value)
+                    new_image___ = elastic_transform(new_image_)
+                    expanded_examples.append(new_image___.reshape(shape))
+                    expanded_labels.append(y)
+            self.__inputs = np.concatenate((self.__inputs, expanded_examples), axis=0)
+            self.__labels = np.concatenate((self.__labels, expanded_labels), axis=0)
+        print self.__labels.size
+        if self.__labels.size > 1 and random == True:
+            res = zip(self.__inputs, self.__labels)
+            np.random.shuffle(res)
+            self.__inputs, self.__labels = zip(*res)
+            self.__inputs = np.array(self.__inputs)
+            self.__labels = np.array(self.__labels)
+        elif random == True:
+            np.random.shuffle(self.__inputs)
+
+    def get_all(self):
+        return self.__inputs, self.__labels
+    
 
     def next_batch(self, batch_size):
         if batch_size > self.__inputs.shape[0]:
@@ -50,11 +113,11 @@ class Dataset(object):
             first_part_y = self.__labels[start:self.__labels.shape[0]]
             batch_size -= first_part_x.shape[0]
 
-            res = zip(self.__inputs, self.__labels)
-            np.random.shuffle(res)
-            self.__inputs, self.__labels = zip(*res)
-            self.__inputs = np.array(self.__inputs)
-            self.__labels = np.array(self.__labels)
+            #res = zip(self.__inputs, self.__labels)
+            #np.random.shuffle(res)
+            #self.__inputs, self.__labels = zip(*res)
+            #self.__inputs = np.array(self.__inputs)
+            #self.__labels = np.array(self.__labels)
             start = 0
             self.__index_in_epoch = 0
             self.__epochs_completed += 1
@@ -83,12 +146,18 @@ class Dataset(object):
 
 
 class MnistDataset(object):
-    def __init__(self, test_filename, train_filename):
+    def __init__(self, test_filename, train_filename, generate_test_set=False):
         train_inputs, train_labels = dataset_from_csv(train_filename, train=True)
-        self.train = Dataset(train_inputs/255., train_labels)
 
-        test_inputs, test_labels = dataset_from_csv(test_filename)
-        self.test = Dataset(test_inputs/255., test_labels)
+        if(generate_test_set == True):
+            self.train = Dataset(train_inputs[:len(train_inputs)*3/4]/255, train_labels[:len(train_inputs)*3/4], expand=True, random=True)
+            self.test =  Dataset(train_inputs[len(train_inputs)*3/4+1:],   train_labels[len(train_inputs)*3/4+1:])
+        else:
+            print('quux')
+            self.train = Dataset(train_inputs/255., train_labels, expand=True, random=True)
+            test_inputs, test_labels = dataset_from_csv(test_filename)
+            self.test = Dataset(test_inputs/255., test_labels)
+            print self.test.inputs.size
 
     def write_results(self, dataset, name='results.csv'):
         results = pd.DataFrame({'ImageId': pd.Series(range(1, len(dataset.labels)+1)), 'Label': pd.Series(dataset.labels)})
--- main.py
+++ main.py
@@ -6,9 +6,13 @@ import pandas as pd
 import tarfile
 import gzip
 import os.path
+import matplotlib.pyplot as plt
 
-n_hidden_1 = 800 # 1st layer number of features
-n_hidden_2 = 200 # 2nd layer number of features
+n_hidden_1 = 4096 # 1st layer number of features
+n_hidden_2 = 4096 # 2nd layer number of features
+n_hidden_3 = 1500 # 2nd layer number of features
+n_hidden_4 = 1000 # 2nd layer number of features
+n_hidden_5 = 500 # 2nd layer number of features
 
 n_input = 784 # MNIST data input (img shape: 28*28)
 n_classes = 10 # MNIST total classes (0-9 digits)
@@ -20,14 +24,23 @@ pkeep = tf.placeholder(tf.float32)
 
 weights = {
     'w1': tf.Variable(tf.random_normal([n_input, n_hidden_1]), name='W1'),
-#    'w2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]), name='W2'),
-    'w3': tf.Variable(tf.random_normal([n_hidden_1, n_classes]), name='W3'),
+    'w2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]), name='W2'),
+    'w23': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3]), name='W23'),
+    'w24': tf.Variable(tf.random_normal([n_hidden_3, n_hidden_4]), name='W24'),
+    'w25': tf.Variable(tf.random_normal([n_hidden_4, n_hidden_5]), name='W25'),
+
+    'w3': tf.Variable(tf.random_normal([n_hidden_2, n_classes]), name='W3'),
 #    'w4': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]), name='W4')
 }
 biases = {
     'b1': tf.Variable(tf.random_normal([n_hidden_1]), name='b1'),
-#    'b2': tf.Variable(tf.random_normal([n_hidden_2]), name='b2'),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2]), name='b2'),
+    'b23': tf.Variable(tf.random_normal([n_hidden_3]), name='b23'),
+    'b24': tf.Variable(tf.random_normal([n_hidden_4]), name='b24'),
+    'b25': tf.Variable(tf.random_normal([n_hidden_5]), name='b25'),
+
     'b3': tf.Variable(tf.random_normal([n_classes]), name='b3'),
+    
 #    'b4': tf.Variable(tf.random_normal([n_hidden_2]), name='b4')
 }
 
@@ -36,18 +49,35 @@ biases = {
 layer_1 = tf.add(tf.matmul(X, weights['w1']), biases['b1'])
 layer_1 = tf.nn.relu(layer_1)
 
-layer_2 = tf.nn.dropout(layer_1, pkeep)
-Ylogits = tf.add(tf.matmul(layer_2, weights['w3']), biases['b3'])
+layer_2 = tf.add(tf.matmul(layer_1, weights['w2']), biases['b2'])
+layer_2 = tf.nn.relu(layer_2)
+
+layer_3 = tf.add(tf.matmul(layer_2, weights['w23']), biases['b23'])
+layer_3 = tf.nn.relu(layer_3)
+#layer_3 = tf.nn.dropout(layer_3, pkeep)
+
+layer_4 = tf.add(tf.matmul(layer_3, weights['w24']), biases['b24'])
+layer_4 = tf.nn.relu(layer_4)
+
+layer_5 = tf.add(tf.matmul(layer_4, weights['w25']), biases['b25'])
+layer_5 = tf.nn.relu(layer_5)
+
+
+layer_6 = tf.nn.dropout(layer_2, pkeep)
+Ylogits = tf.add(tf.matmul(layer_6, weights['w3']), biases['b3'])
 Y = tf.nn.softmax(Ylogits)
 
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Y_)
-cross_entropy = tf.reduce_mean(cross_entropy) *100
+#cross_entropy = cross_entropy + 0.01*tf.nn.l2_loss(weights['w3'])
+cross_entropy = tf.reduce_mean(cross_entropy)
 
 correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 predictions = tf.argmax(Y, 1)
 
-train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+#train_step = tf.train.GradientDescentOptimizer(lr).minimize(cross_entropy)
+#train_step = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cross_entropy)
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
@@ -67,26 +97,51 @@ if(not os.path.isfile('test/train.csv')):
     with open('test/train.csv', 'wb') as f:
         f.write(file_content)
 
-
-data = MnistDataset(test_filename = 'test/test.csv', train_filename='test/train.csv')
-for i in range(1,5000):
-    # data.train.next_batch    max_learning_rate = 0.003
-    max_learning_rate = 0.003
+history = {'acc': [], 'val_acc': [], 'loss': [], 'val_loss': []}
+data = MnistDataset(test_filename = 'test/test.csv', train_filename='test/train.csv', generate_test_set=False)
+i = 0
+while(True):
+    i = i + 1
+    #data.train.next_batch
+    max_learning_rate = 0.01
+    max_learning_rate = 0.001
     min_learning_rate = 0.0001
-    decay_speed = 2000.0
+    decay_speed = 1e-6
     learning_rate = min_learning_rate + (max_learning_rate - min_learning_rate) * math.exp(-i/decay_speed)
 
     batch_X, batch_Y = data.train.next_batch(1000)
-    #batch_X = batch_X.reshape((100, batch_X.shape[1]))
 
-    #batch_Y = batch_Y.reshape((100,batch_Y.shape[1]))
     a, c = sess.run([accuracy, cross_entropy], {X: batch_X, Y_: batch_Y, pkeep: 1.0})
-    print(str(data.train.epoch) + ": training accuracy:" + str(a) + " training loss: " + str(c) )
-    sess.run(train_step, {X: batch_X, Y_: batch_Y, lr: learning_rate, pkeep: 0.75})
+    prev = str(data.train.epoch) + ": training accuracy:" + str(a) + " training loss: " + str(c)
+    print(prev)
+    history['acc'].append(a)
+    history['loss'].append(c)
+    #if(generate_test_set and i % 100 == 0):
+    #real_X, real_Y = data.test.get_all()
+    #acc, ccc = sess.run([accuracy, cross_entropy], {X: real_X, Y_: real_Y, pkeep: 1.0})
 
+    #print(prev +  ": test accuracy:" + str(acc) + " test loss: " + str(ccc) + "\t LR" + str(learning_rate) )
+    #history['val_acc'].append(acc)
+    #history['val_loss'].append(ccc)
+    learning_rate = 0.5
+    sess.run(train_step, {X: batch_X, Y_: batch_Y, lr: learning_rate, pkeep: 0.5})
+    if data.train.epoch == 40:
+        break
+
+# generate plots
+fig, ax = plt.subplots(2,1)
+ax[0].plot(history['loss'], color='b', label="Training loss")
+ax[0].plot(history['val_loss'], color='r', label='Validation loss', axes = ax[0])
+legend = ax[0].legend(loc='best', shadow=True)
+
+ax[1].plot(history['acc'], color='b', label="Training accuracy")
+ax[1].plot(history['val_acc'], color='r', label="Validation accuracy")
+legend = ax[1].legend(loc='best', shadow=True)
+plt.show()
 # Run test data
 batch_X = data.test.inputs
 p = sess.run([predictions], {X: batch_X, pkeep: 1.0})
 results = pd.DataFrame({'ImageId': pd.Series(range(1, len(p[0]) + 1)), 'Label': pd.Series(p[0])})
+
 print(p)
 results.to_csv('results.csv', index=False)
